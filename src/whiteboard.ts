76c7e6ab95eef0b5c9e9879237844eb54cc522a0
Khoi Nguyen <khoi@nguyen.me.uk>
2022-12-26 03:53:51 +0100

    Start conversion to typescript

---
+++ src/whiteboard.ts
@@ -0,0 +1,153 @@
+type Point = [number, number];
+
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+
+type Color = RGB | RGBA | HEX;
+
+type Mode = "draw" | "erase";
+
+interface Stroke {
+  color: string;
+  lineWidth: number;
+  path: Path2D;
+  points: Point[];
+}
+
+class Whiteboard {
+
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  private strokes: Stroke[];
+
+  color: Color = "#255994";
+  lineWidth: number = 2;
+  mode: Mode = "draw";
+  isActive: boolean = false;
+
+  width: number;
+  height: number;
+
+  /**
+   * Add a point to the last stroke
+   * @param point Point to add
+   */
+  addPoint(point: Point): void {
+    const stroke = this.strokes[this.strokes.length - 1];
+    stroke.points.push(point);
+    stroke.path.lineTo(...point);
+    this.drawStroke(stroke);
+  }
+
+  clearBoard(): void {
+    this.context.clearRect(0, 0, this.width, this.height)
+  }
+
+  /**
+   * Create a canvas with its 2D context
+   * @param width Width of the canvas
+   * @param height Height of the canvas
+   */
+  constructor(width: number, height: number): void {
+    this.canvas = document.createElement("canvas");
+    this.context = this.canvas.getContext("2d")!;
+    this.canvas.addEventListener("mousedown", this.onMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.onMouseMove.bind(this));
+    this.canvas.addEventListener("mouseup", this.onMouseUp.bind(this));
+    this.width = width;
+    this.height = height;
+  }
+
+  /**
+   * Draw a stroke on the canvas
+   * @param stroke Stroke to draw on the canvas
+   */
+  drawStroke(stroke: Stroke): void {
+    this.context.fillStyle = stroke.color;
+    this.context.strokeStyle = stroke.color;
+    this.context.lineCap = "round";
+    this.context.lineWidth = stroke.lineWidth;
+    this.context.stroke(stroke.path);
+  }
+
+  /**
+   * Erase all strokes that contains a certain point
+   * @param point Point
+   */
+  eraseStroke(point: Point): void {
+    for (let i = 0; i < this.strokes.length; i++) {
+      const stroke = this.strokes[i];
+      if (this.context.isPointInPath(stroke.path, ...point) {
+        this.strokes.splice(i, 1);
+        this.redraw();
+      }
+    }
+  }
+
+  /**
+   * Redraw all the strokes on the canvas
+   */
+  redraw(): void {
+    this.clearBoard();
+    this.strokes.forEach((stroke) => this.drawStroke(stroke));
+  }
+  
+  /**
+   * Mouse down event listener
+   * @param event Mouse event
+   */
+  onMouseDown(event: MouseEvent): void {
+    const leftClick = event.button === 0 || event.button === 1;
+    const rightClick = event.button === 2;
+    if (leftClick || rightClick) {
+      this.isActive = true;
+      if (rightClick) {
+        this.mode = "erase";
+      }
+    }
+  }
+  
+  /**
+   * Mouse move event listener
+   * @param event Mouse event
+   */
+  onMouseMove(event: MouseEvent): void {
+    if (!this.isActive) {
+      return;
+    }
+    const point: Point = [event.clientX, event.clientY];
+    if (this.mode === "draw") {
+      this.addPoint(point);
+    } else if (this.mode === "erase") {
+      this.eraseStroke(point);
+    }
+  }
+  
+  /**
+   * Mouse up event listener
+   * @param event Mouse event
+   */
+  onMouseUp(event: MouseEvent): void {
+    this.isActive = false;
+    if (this.mode === "draw") {
+      this.startStroke();
+      this.redraw();
+    }
+    if (event.button === 2) {
+      this.mode = "draw";
+    }
+  }
+
+  /**
+   * Start a new stroke
+   */
+  startStroke(): void {
+    this.strokes.push({
+      color: this.color,
+      lineWidth: this.lineWidth,
+      path: new Path2D(),
+      points: [],
+    });
+  }
+}
