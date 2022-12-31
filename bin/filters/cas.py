7082b38bfdf6b33757b126357f456843cc4b08bf
Khoi Nguyen <khoi@nguyen.me.uk>
2022-12-31 06:54:15 +0100

    Improve exec so that it can access sympy and np

--- bin/filters/cas.py
+++ bin/filters/cas.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python
 
 import ast
-import textwrap
+import importlib
 
+import numpy as np
 import panflute as pf
 import sympy
 
 stack = []
 
+_global = importlib.import_module("sympy").__dict__
+_global.update({"np": np})
+for letter in ["x", "y", "z", "t"]:
+    _global[letter] = sympy.symbols(letter)
+for letter in ["k", "m", "n"]:
+    _global[letter] = sympy.symbols(letter, integer=True)
+
 
 def exec_then_eval(code):
+    global _global
     block = ast.parse(code, mode="exec")
     last = ast.Expression(block.body.pop().value)
-    _globals, _locals = {}, {}
-    exec(compile(block, "<string>", mode="exec"), _globals, _locals)
-    return eval(compile(last, "<string>", mode="eval"), _globals, _locals)
+    exec(compile(block, "<string>", mode="exec"), _global, _global)
+    return eval(compile(last, "<string>", mode="eval"), _global, _global)
 
 
 def cas(element, doc):
     global stack
     if type(element) in [pf.Code, pf.CodeBlock] and "sympy" in element.classes:
-        code = textwrap.dedent(
-            """
-            import numpy as np
-            from sympy import *
-            x, y, z, t = symbols("x y z t")
-            k, m, n = symbols("k m n", integer=True)
-        """
-        )
+        code = ""
         for index, block in enumerate(stack):
             code += block["code"] + "\n"
             block["stack"] -= 1
@@ -35,7 +36,7 @@ def cas(element, doc):
                 del stack[index]
         if element.attributes.get("stack"):
             stack.append(
-                {"code": element.text, "stack": int(element.attributes.get("stack"))}
+                {"code": element.text, "stack": int(element.attributes["stack"])}
             )
         code += element.text
         result = exec_then_eval(code)
