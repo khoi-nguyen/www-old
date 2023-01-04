3f82641d3f58d14d0653f651e3d21c8136e80276
Khoi Nguyen <khoi@nguyen.me.uk>
2023-01-04 03:10:18 +0100

    Move envs in meta.yaml and improve type hints

--- bin/filters/environments.py
+++ bin/filters/environments.py
@@ -2,99 +2,42 @@
 
 import panflute as pf
 
-proofcount = 0
-
-envs = {
-    "algorithm": {
-        "name": "Algorithm",
-        "classes": ["bg-success", "text-dark", "text-white"],
-        "icon": "fa-solid fa-code",
-    },
-    "alertblock": {
-        "name": "",
-        "classes": ["bg-danger", "text-dark", "text-white"],
-    },
-    "block": {
-        "name": "",
-        "classes": ["bg-primary", "text-dark", "text-white"],
-    },
-    "exampleblock": {
-        "name": "",
-        "classes": ["bg-success", "text-dark", "text-white"],
-    },
-    "definition": {
-        "name": "Definition",
-        "classes": ["bg-success", "text-dark", "text-white"],
-        "icon": "fa-solid fa-book",
-    },
-    "example": {
-        "name": "Example",
-        "classes": ["bg-success", "text-dark", "text-white"],
-        "icon": "fa-solid fa-pen-to-square",
-    },
-    "exercise": {
-        "name": "Exercise",
-        "classes": ["bg-success", "text-dark", "text-white"],
-        "icon": "fa-solid fa-pencil",
-    },
-    "proposition": {
-        "name": "Proposition",
-        "classes": ["bg-primary", "text-dark", "text-white"],
-        "icon": "fa-solid fa-book",
-    },
-    "question": {
-        "name": "Question",
-        "classes": ["bg-primary", "text-dark", "text-white"],
-        "icon": "fa-solid fa-circle-question",
-    },
-    "remark": {
-        "name": "Remark",
-        "classes": ["bg-danger", "text-dark", "text-white"],
-    },
-    "task": {
-        "name": "Task",
-        "classes": ["bg-success", "text-dark", "text-white"],
-        "icon": "fa-solid fa-laptop",
-    },
-    "theorem": {
-        "name": "Theorem",
-        "classes": ["bg-primary", "text-dark", "text-white"],
-        "icon": "fa-solid fa-book",
-    },
-}
+proofcount: int = 0
 
 
-def environments(element, doc):
-    if type(element) == pf.Div and "solution" in element.classes:
+def environments(element: pf.Element, doc: pf.Doc):
+    if not isinstance(element, pf.Div):
+        return element
+    envs: dict[str, dict] = getattr(doc, "get_metadata")("envs", {})
+    if "solution" in element.classes:
         return collapsable_card(element, element.attributes.get("title", "Solution"))
-    if type(element) == pf.Div and "proof" in element.classes:
+    if "proof" in element.classes:
         return collapsable_card(element, "Proof")
-    elif type(element) == pf.Div:
-        name = list(set(envs.keys()) & set(element.classes))
-        if name:
-            env = envs[name[0]]
-            icon = ""
-            if "icon" in env:
-                icon = env["icon"]
-            if "icon" in element.attributes:
-                icon = element.attributes["icon"]
-            icon = f"""<i class="{icon}"></i>""" if "icon" in env else ""
-            header = f"""
-            <h5 class="card-header {' '.join(env["classes"])}">
-              {icon}
-              <strong>{env["name"]}</strong>
-              {'(' if "title" in element.attributes and env["name"] else ""}
-              {element.attributes.get("title", "")}
-              {')' if "title" in element.attributes and env["name"] else ""}
-            </h5>
-          """
-            header = pf.RawBlock(header, format="html")
-            element.classes.append("card-body")
-            container_classes = ["card"]
-            if "fragment" in element.classes:
-                element.classes.remove("fragment")
-                container_classes.append("fragment")
-            return pf.Div(header, element, classes=container_classes)
+    name = list(set(envs.keys()) & set(element.classes))
+    if name:
+        env = envs[name[0]]
+        icon = ""
+        if "icon" in env:
+            icon = env["icon"]
+        if "icon" in element.attributes:
+            icon = element.attributes["icon"]
+        icon = f"""<i class="{icon}"></i>""" if "icon" in env else ""
+        header = f"""
+        <h5 class="card-header {' '.join(env["classes"])}">
+          {icon}
+          <strong>{env.get("name")}</strong>
+          {'(' if "title" in element.attributes and env.get("name") else ""}
+          {element.attributes.get("title", "")}
+          {')' if "title" in element.attributes and env.get("name") else ""}
+        </h5>
+      """
+        header = pf.RawBlock(header, format="html")
+        element.classes.append("card-body")
+        container_classes = ["card"]
+        if "fragment" in element.classes:
+            element.classes.remove("fragment")
+            container_classes.append("fragment")
+        return pf.Div(header, element, classes=container_classes)
 
 
 def collapsable_card(element, title="Solution"):
