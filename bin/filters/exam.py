e510d2a83850d1010d693f252e3030b07b2cad5b
Khoi Nguyen <khoi@nguyen.me.uk>
2023-01-04 17:07:36 +0100

    Improve type annotations in filter

--- bin/filters/exam.py
+++ bin/filters/exam.py
@@ -3,10 +3,19 @@
 import panflute as pf
 
 
-def exam(element, doc):
+def exam(element: pf.Element, doc: pf.Doc) -> None | pf.Element:
+    """Create an exam class LaTeX document
+
+    - Convert headers to questsion/parts
+    - Convert divs to environments when applicable
+    """
+
+    if doc.format != "latex":
+        return None
+
     if isinstance(element, pf.Header):
-        cmd = ["", "titledquestion", "part"]
-        tex = "\\" + cmd[element.level]
+        cmd: list[str] = ["", "titledquestion", "part"]
+        tex: str = "\\" + cmd[element.level]
         if element.level == 1:
             if pf.stringify(element):
                 tex += "{" + pf.stringify(element) + "}"
@@ -16,8 +25,9 @@ def exam(element, doc):
             tex += f"[{element.attributes['marks']}]"
         question = pf.RawBlock(tex, format="latex")
         return question
+
     if isinstance(element, pf.Div) and len(element.classes) == 1:
-        env = element.classes[0]
+        env: str = element.classes[0]
         begin, end = r"\begin{" + env + "}", r"\end{" + env + "}"
         element.content.insert(0, pf.RawBlock(begin, format="latex"))
         element.content.append(pf.RawBlock(end, format="latex"))
