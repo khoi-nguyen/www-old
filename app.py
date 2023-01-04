50c4af14e4144d72c6d8112bc4047a6ae004bc6c
Khoi Nguyen <khoi@nguyen.me.uk>
2023-01-04 05:37:45 +0100

    Reorganise app.py

--- app.py
+++ app.py
@@ -9,16 +9,20 @@ DEBUG = os.environ.get("ENVIRONMENT", "") != "production"
 
 app = flask.Flask(__name__, template_folder="templates")
 app.secret_key = os.environ.get("SECRET_KEY", "hello").encode("UTF-8")
-socketio = flask_socketio.SocketIO(app, logger=DEBUG)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+# ================
+# Login and logout
+# ================
+
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
 
-class User(flask_login.UserMixin):
-    id = "admin"
-
-
-admin = User()
+admin = flask_login.UserMixin()
+setattr(admin, "id", "admin")
 
 
 @login_manager.user_loader
@@ -26,33 +30,6 @@ def load_user(user_id):
     if user_id == "admin":
         return admin
     return None
-
-
-# Configuring Jinja
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_page("build/404.html", {"error": str(error)})
-
-
-@app.route("/favicon.ico")
-def favicon():
-    static = os.path.join(app.root_path, "static")
-    return flask.send_from_directory(
-        static, "favicon.ico", mimetype="image/vnd.microsoft.icon"
-    )
-
-
-@app.route("/socketio/<path:url>", methods=["POST"])
-@flask_login.login_required
-def socket(url: str = ""):
-    json = flask.request.get_json() or {}
-    json.update({"url": "/" + url})
-    socketio.emit("changeReceived", json, broadcast=True)
-    return flask.jsonify({"success": True})
 
 
 @app.route("/login", methods=["POST"])
@@ -73,6 +50,22 @@ def logout():
     return flask.jsonify({"success": True})
 
 
+# ===========
+# Whiteboards
+# ===========
+
+socketio = flask_socketio.SocketIO(app, logger=DEBUG)
+
+
+@app.route("/socketio/<path:url>", methods=["POST"])
+@flask_login.login_required
+def socket(url: str = ""):
+    json = flask.request.get_json() or {}
+    json.update({"url": "/" + url})
+    socketio.emit("changeReceived", json, broadcast=True)
+    return flask.jsonify({"success": True})
+
+
 @app.route("/boards/<path:url>", methods=["POST"])
 @flask_login.login_required
 def save_board(url: str = ""):
@@ -82,15 +75,35 @@ def save_board(url: str = ""):
     return flask.jsonify({"success": True, "boards": boards})
 
 
+def clean(boards):
+    for slide in boards:
+        for board in slide:
+            for i, stroke in enumerate(board):
+                if len(stroke["points"]) == 0:
+                    del board[i]
+    return boards
+
+
+# =============
+# Static routes
+# =============
+
+
 @app.route("/")
 @app.route("/<path:url>")
 def default_route(url: str = ""):
+    if url == "favicon.ico":
+        return flask.send_file("static/favicon.ico")
+
     if url.endswith("/") or not url:
         url += "index.html"
     elif "." not in url:
         url += ".html"
+
+    # Protect metadata files and send board files instead
     if url.endswith(".json") and os.path.exists(url):
         return flask.send_file(url)
+
     url = "build/" + url
     if not os.path.exists(url) or url.endswith(".json"):
         flask.abort(404)
@@ -99,14 +112,13 @@ def default_route(url: str = ""):
     return flask.send_file(url)
 
 
-def render_page(path: str, variables: dict = {}) -> str:
+def render_page(path: str) -> str:
     def file_contents(path: str) -> str:
         with open(path) as f:
             return f.read()
 
     data = json.loads(file_contents(path.replace(".html", ".json")))
     data["user"] = flask_login.current_user
-    data.update(variables)
     content = flask.render_template_string(file_contents(path), **data)
     data.update({"content": content})
     if data.get("private") and not getattr(data["user"], "is_authenticated"):
@@ -115,13 +127,15 @@ def render_page(path: str, variables: dict = {}) -> str:
     return flask.render_template(template_file, **data)
 
 
-def clean(boards):
-    for slide in boards:
-        for board in slide:
-            for i, stroke in enumerate(board):
-                if len(stroke["points"]) == 0:
-                    del board[i]
-    return boards
+# ==============
+# Error handling
+# ==============
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    del error
+    return render_page("build/404.html")
 
 
 if __name__ == "__main__":
