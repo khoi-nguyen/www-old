48b7aa3b2f95899f9e4bd0a701e3c13aa59938a4
Khoi Nguyen <khoi@nguyen.me.uk>
2023-01-08 20:47:53 +0100

    Add Julia support

--- bin/filters/cas.py
+++ bin/filters/cas.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-import ast
-import importlib
+import os
+import subprocess
+import tempfile
+import textwrap
+import typing
 
-import numpy as np
 import panflute as pf
 import sympy
-import typing
 
 
 class StackElement(typing.TypedDict):
@@ -14,30 +15,58 @@ class StackElement(typing.TypedDict):
     stack: int
 
 
+class Env(typing.TypedDict):
+    code: str
+    cmd: list[str]
+    println: str
+
+
 stack: list[StackElement] = []
-CLASSES: set = {"sympy", "eval"}
+CONFIG: dict[str, Env] = {
+    "python": {
+        "code": textwrap.dedent(
+            """
+            import numpy as np
+            from sympy import *
+            x, y, z, t = symbols("x y z t")
+            k, m, n = symbols("k m n", integer=True)
+        """
+        ),
+        "cmd": ["env", "python"],
+        "println": "print(%s)",
+    },
+    "julia": {
+        "code": "",
+        "cmd": ["env", "julia", "--project=."],
+        "println": "println(%s)",
+    },
+}
+LANG = typing.Literal["python", "julia"]
 
-_global = importlib.import_module("sympy").__dict__
-_global.update({"np": np})
-for letter in ["x", "y", "z", "t"]:
-    _global[letter] = sympy.symbols(letter)
-for letter in ["k", "m", "n"]:
-    _global[letter] = sympy.symbols(letter, integer=True)
 
-
-def exec_then_eval(code: str) -> str:
-    global _global
-    block = ast.parse(code, mode="exec")
-    last = ast.Expression(getattr(block.body.pop(), "value"))
-    exec(compile(block, "<string>", mode="exec"), _global, _global)
-    return eval(compile(last, "<string>", mode="eval"), _global, _global)
+def exec_then_eval(code: str, lang: LANG) -> str:
+    name: str = ""
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp:
+        name = tmp.name
+        lines = code.split("\n")
+        lines[-1] = CONFIG[lang]["println"] % lines[-1]
+        tmp.write(CONFIG[lang]["code"] + "\n".join(lines))
+        cmd = CONFIG[lang]["cmd"] + [name]
+    out = subprocess.run(cmd, capture_output=True, text=True).stdout
+    os.remove(name)
+    try:
+        expr = sympy.sympify(out)
+        return sympy.latex(expr)
+    except:
+        return out
 
 
 def cas(element: pf.Element, doc: pf.Doc) -> None | pf.Element:
     del doc
-    if not (
-        isinstance(element, pf.Code) or isinstance(element, pf.CodeBlock)
-    ) or not CLASSES & set(element.classes):
+    if (
+        not (isinstance(element, pf.Code) or isinstance(element, pf.CodeBlock))
+        or "eval" not in element.classes
+    ):
         return None
 
     # Reuse code from previous blocks if necessary
@@ -53,10 +82,11 @@ def cas(element: pf.Element, doc: pf.Doc) -> None | pf.Element:
         stack.append({"code": element.text, "stack": int(element.attributes["stack"])})
 
     code += element.text
-    result = exec_then_eval(code)
+    lang = "julia" if "julia" in element.classes else "python"
+    result = exec_then_eval(code, lang)
     if type(element) == pf.Code:
-        return pf.Math(sympy.latex(result), format="InlineMath")
-    return pf.Para(pf.Math(sympy.latex(result), format="DisplayMath"))
+        return pf.Math(result, format="InlineMath")
+    return pf.Para(pf.Math(result, format="DisplayMath"))
 
 
 if __name__ == "__main__":
