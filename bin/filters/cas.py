c73baad25c4c43896c48ee3f950e435b74e1bc83
Khoi Nguyen <khoi@nguyen.me.uk>
2023-01-08 17:51:41 +0100

    Rewrite cas.py

--- bin/filters/cas.py
+++ bin/filters/cas.py
@@ -6,8 +6,16 @@ import importlib
 import numpy as np
 import panflute as pf
 import sympy
+import typing
 
-stack = []
+
+class StackElement(typing.TypedDict):
+    code: str
+    stack: int
+
+
+stack: list[StackElement] = []
+CLASSES: set = {"sympy", "eval"}
 
 _global = importlib.import_module("sympy").__dict__
 _global.update({"np": np})
@@ -17,32 +25,38 @@ for letter in ["k", "m", "n"]:
     _global[letter] = sympy.symbols(letter, integer=True)
 
 
-def exec_then_eval(code):
+def exec_then_eval(code: str) -> str:
     global _global
     block = ast.parse(code, mode="exec")
-    last = ast.Expression(block.body.pop().value)
+    last = ast.Expression(getattr(block.body.pop(), "value"))
     exec(compile(block, "<string>", mode="exec"), _global, _global)
     return eval(compile(last, "<string>", mode="eval"), _global, _global)
 
 
-def cas(element, doc):
-    global stack
-    if type(element) in [pf.Code, pf.CodeBlock] and "sympy" in element.classes:
-        code = ""
-        for index, block in enumerate(stack):
-            code += block["code"] + "\n"
-            block["stack"] -= 1
-            if not block["stack"]:
-                del stack[index]
-        if element.attributes.get("stack"):
-            stack.append(
-                {"code": element.text, "stack": int(element.attributes["stack"])}
-            )
-        code += element.text
-        result = exec_then_eval(code)
-        if type(element) == pf.Code:
-            return pf.Math(sympy.latex(result), format="InlineMath")
-        return pf.Para(pf.Math(sympy.latex(result), format="DisplayMath"))
+def cas(element: pf.Element, doc: pf.Doc) -> None | pf.Element:
+    del doc
+    if not (
+        isinstance(element, pf.Code) or isinstance(element, pf.CodeBlock)
+    ) or not CLASSES & set(element.classes):
+        return None
+
+    # Reuse code from previous blocks if necessary
+    code: str = ""
+    for index, block in enumerate(stack):
+        code += block["code"] + "\n"
+        block["stack"] -= 1
+        if not block["stack"]:
+            del stack[index]
+
+    # Save code for late use if necessary
+    if element.attributes.get("stack"):
+        stack.append({"code": element.text, "stack": int(element.attributes["stack"])})
+
+    code += element.text
+    result = exec_then_eval(code)
+    if type(element) == pf.Code:
+        return pf.Math(sympy.latex(result), format="InlineMath")
+    return pf.Para(pf.Math(sympy.latex(result), format="DisplayMath"))
 
 
 if __name__ == "__main__":
