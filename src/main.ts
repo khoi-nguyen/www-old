b0acbb616bdbb0bcb9495dd102172be9e2124e61
Khoi Nguyen <khoi@nguyen.me.uk>
2023-03-06 00:03:51 +0100

    Improve code quality

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import Clock from "./elements/Clock.ce.vue";
 import PdfReader from "./elements/PdfReader.ce.vue";
 import WhiteboardPlugin from "./WhiteboardPlugin";
 import JSXBoard from "./jsxgraph";
+import katex from "katex";
+import renderMathInElement from "katex/dist/contrib/auto-render.mjs";
 
 const ClockElement = defineCustomElement(Clock);
 customElements.define("ticking-clock", ClockElement);
@@ -10,19 +12,16 @@ customElements.define("ticking-clock", ClockElement);
 const PdfReaderElement = defineCustomElement(PdfReader);
 customElements.define("pdf-reader", PdfReaderElement);
 
-import katex from "katex";
-import renderMathInElement from "katex/dist/contrib/auto-render.mjs";
-
 (<any>window).RevealWhiteboard = new WhiteboardPlugin();
 (<any>window).JSXBoard = JSXBoard;
 (<any>window).renderMathInElement = renderMathInElement;
 
 document.addEventListener("DOMContentLoaded", function () {
-  var mathElements = document.getElementsByClassName("math");
-  var macros = [];
-  for (var i = 0; i < mathElements.length; i++) {
-    var texText = mathElements[i].firstChild;
-    if (mathElements[i].tagName == "SPAN") {
+  const mathElements = document.getElementsByClassName("math");
+  const macros = [];
+  for (let i = 0; i < mathElements.length; i++) {
+    const texText = mathElements[i].firstChild;
+    if (mathElements[i].tagName == "SPAN" && texText && "data" in texText) {
       katex.render(texText.data, mathElements[i], {
         displayMode: mathElements[i].classList.contains("display"),
         throwOnError: false,
