fb1dc96a0c0e4cc537a03a56a0e14de0306ed56f
Khoi Nguyen <khoi@nguyen.me.uk>
2023-04-02 22:24:32 +0200

    Better part handling

--- bin/filters/exam.py
+++ bin/filters/exam.py
@@ -3,13 +3,20 @@
 import panflute as pf
 
 
+first_part = True
+close_parts = False
+
+
 def finalize(doc: pf.Doc) -> None:
     if doc.format != "latex":
         return None
 
-    tex = pf.RawBlock(r"\end{parts}", format="latex")
-    doc.content.append(tex)
     doc.content.pop(0)
+
+    global close_parts
+    if close_parts:
+        tex = pf.RawBlock(r"\end{parts}", format="latex")
+        doc.content.append(tex)
 
 
 def exam(element: pf.Element, doc: pf.Doc) -> None | pf.Element:
@@ -22,17 +29,29 @@ def exam(element: pf.Element, doc: pf.Doc) -> None | pf.Element:
     if doc.format != "latex":
         return None
 
+    global first_part, close_parts
+
     if isinstance(element, pf.Header) and element.level == 1:
-        tex = r"\question"
+        tex = ""
+        if close_parts:
+            tex = r"\end{parts}"
+            close_parts = False
+            first_part = True
         if pf.stringify(element):
-            tex = r"\titledquestion{" + pf.stringify(element) + "}"
+            tex += r"\titledquestion{" + pf.stringify(element) + "}"
+        else:
+            tex += r"\question"
         if "marks" in element.attributes:
             tex += f"[{element.attributes['marks']}]"
-        tex += r"\begin{parts}"
         return pf.RawBlock(tex, format="latex")
 
     if isinstance(element, pf.Header) and element.level == 2:
-        tex = r"\part"
+        tex = ""
+        if first_part:
+            tex += r"\begin{parts}"
+            first_part = False
+            close_parts = True
+        tex += r"\part"
         if "marks" in element.attributes:
             tex += f"[{element.attributes['marks']}]"
         return pf.RawBlock(tex, format="latex")
